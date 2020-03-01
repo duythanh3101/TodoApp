92432090552552b4357c7ec50c8418c86d2cd3fc
songoku311 <duythanh3101@gmail.com>
2020-03-02 03:13:17 +0700

    Dismiss keyboard when clicking outside textinput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, ListView, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ListView, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -36,20 +36,26 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Header/>
-      <View style={styles.content}>
-        <AddTodo submitHandler={buttonPressHandler}/>
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({item}) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />
+  <TouchableWithoutFeedback
+    onPress={ () => {
+      Keyboard.dismiss();
+    }}
+    >
+      <View style={styles.container}>
+        <Header/>
+        <View style={styles.content}>
+          <AddTodo submitHandler={buttonPressHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
